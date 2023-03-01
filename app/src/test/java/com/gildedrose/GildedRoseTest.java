e66c69de44760e53e2041e6cb38584b38b6087ce
Ana Montero <anamongar@gmail.com>
2023-03-02 00:02:58 +0100

    Created first tests

--- app/src/test/java/com/gildedrose/GildedRoseTest.java
+++ app/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,37 @@
 package com.gildedrose;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    @ParameterizedTest
+    @CsvSource({
+            "Cheese, 5, 3, 1, 2",
+    })
+    void updateQualityCorrectly(String name, int sellIn, int quality, int days, int expectedQuality) {
+        Item[] items = new Item[] { new Item(name, sellIn, quality) };
         GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        for (int i = 0; i < days; i++) {
+            app.updateQuality();
+        }
+        int updatedQuality = items[0].quality;
+        assertEquals(expectedQuality, updatedQuality);
     }
 
+    @ParameterizedTest
+    @CsvSource({
+            "Cheese, 5, 3, 1, 4",
+    })
+    void updateSellInCorrectly(String name, int sellIn, int quality, int days, int expectedQuality) {
+        Item[] items = new Item[] { new Item(name, sellIn, quality) };
+        GildedRose app = new GildedRose(items);
+        for (int i = 0; i < days; i++) {
+            app.updateQuality();
+        }
+        int updatedSellIn = items[0].sellIn;
+        assertEquals(expectedQuality, updatedSellIn);
+    }
 }
